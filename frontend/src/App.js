adf1fc998745bfc08e287c19ff691d1f1c112be8
Aishwarya <aishujoshi121@gmail.com>
2026-09-07 15:20:52 +0530

    app.js fully restored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,61 +13,61 @@ import Role from "./Components/Role";
 
 
 
-// function App() {
-
-
-//   const [alert, setAlert] = useState(null);
-
-//   const showAlert = (message, type) => {
-//     setAlert({
-//       msg: message,
-//       type: type
-//     });
-
-//     setTimeout(() => {
-//       setAlert(null);
-//     }, 1500);
-//   }
-
-//   return (
-//     <Router>
-//       <Routes>
-
-//         {/* Login page */}
-//         <Route path="/login" element={<Login showAlert={showAlert} />} />
-
-//         {/* Layout */}
-//         <Route element={<Layout alert={alert} showAlert={showAlert} />}>
-//           <Route path="/" element={<Dashboard showAlert={showAlert} />} />
-//           <Route path="/user" element={<User showAlert={showAlert} />} />
-//           <Route path="/role" element={<Role showAlert={showAlert} />} />
-//           <Route path="/dashboard" element={<Dashboard showAlert={showAlert} />} />
-//           <Route path="/office" element={<Office showAlert={showAlert} />} />
-//           <Route path="/builty" element={<Builty showAlert={showAlert} />} />
-//           <Route path="/city" element={<City showAlert={showAlert} />} />
-//           <Route path="/builtyreport" element={<BuiltyReport showAlert={showAlert} />} />
-//           <Route path="/customer" element={<Customer showAlert={showAlert} />} />
-
-//         </Route>
-
-//       </Routes>
-
-//     </Router>
-//   );
-// }
-
-
 function App() {
+
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
   return (
     <Router>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Dashboard />} />
+
+        {/* Login page */}
+        <Route path="/login" element={<Login showAlert={showAlert} />} />
+
+        {/* Layout */}
+        <Route element={<Layout alert={alert} showAlert={showAlert} />}>
+          <Route path="/" element={<Dashboard showAlert={showAlert} />} />
+          <Route path="/user" element={<User showAlert={showAlert} />} />
+          <Route path="/role" element={<Role showAlert={showAlert} />} />
+          <Route path="/dashboard" element={<Dashboard showAlert={showAlert} />} />
+          <Route path="/office" element={<Office showAlert={showAlert} />} />
+          <Route path="/builty" element={<Builty showAlert={showAlert} />} />
+          <Route path="/city" element={<City showAlert={showAlert} />} />
+          <Route path="/builtyreport" element={<BuiltyReport showAlert={showAlert} />} />
+          <Route path="/customer" element={<Customer showAlert={showAlert} />} />
+
         </Route>
+
       </Routes>
+
     </Router>
   );
 }
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route element={<Layout />}>
+//           <Route path="/" element={<Dashboard />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
 
 
 export default App;
